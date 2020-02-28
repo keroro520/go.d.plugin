0f82cc34001c101a3f3ddd6a41cd89cfb6d6c3f1
keroro520 <keroroxx520@gmail.com>
2020-02-28 12:15:03 +0800

    feat: CGO_ENABLED=1

--- hack/go-build.sh
+++ hack/go-build.sh
@@ -3,7 +3,8 @@
 set -e
 
 WHICH="$1"
-PLATFORMS=(darwin/386 darwin/amd64 freebsd/386 freebsd/amd64 freebsd/arm linux/386 linux/amd64 linux/arm linux/arm64 linux/ppc64 linux/ppc64le linux/mips linux/mipsle linux/mips64 linux/mips64le)
+# PLATFORMS=(darwin/386 darwin/amd64 freebsd/386 freebsd/amd64 freebsd/arm linux/386 linux/amd64 linux/arm linux/arm64 linux/ppc64 linux/ppc64le linux/mips linux/mipsle linux/mips64 linux/mips64le)
+PLATFORMS=(linux/amd64)
 
 VERSION="${TRAVIS_TAG}"
 : "${VERSION:=$(git describe --tags --always --dirty)}"
@@ -16,14 +17,14 @@ if [[ "${WHICH}" != "all" ]]; then
 	eval $(go env | grep -e "GOHOSTOS" -e "GOHOSTARCH")
 	: "${GOOS:=${GOHOSTOS}}"
 	: "${GOARCH:=${GOHOSTARCH}}"
-	CGO_ENABLED=0 GOOS=${GOOS} GOARCH=${GOARCH} go build ${GOFLAGS} -ldflags "${GLDFLAGS}" -o bin/godplugin github.com/netdata/go.d.plugin/cmd/godplugin
+	CGO_ENABLED=1 GOOS=${GOOS} GOARCH=${GOARCH} go build ${GOFLAGS} -ldflags "${GLDFLAGS}" -o bin/godplugin github.com/netdata/go.d.plugin/cmd/godplugin
 else
 	for PLATFORM in "${PLATFORMS[@]}"; do
 		PLTFRM_SPLT=(${PLATFORM//\// })
 		GOOS=${PLTFRM_SPLT[0]}
 		GOARCH=${PLTFRM_SPLT[1]}
 		FILE="bin/go.d.plugin-${VERSION}.${GOOS}-${GOARCH}"
-		CGO_ENABLED=0 GOOS=${GOOS} GOARCH=${GOARCH} go build ${GOFLAGS} -ldflags "${GLDFLAGS}" -o "${FILE}" github.com/netdata/go.d.plugin/cmd/godplugin
+		CGO_ENABLED=1 GOOS=${GOOS} GOARCH=${GOARCH} go build ${GOFLAGS} -ldflags "${GLDFLAGS}" -o "${FILE}" github.com/netdata/go.d.plugin/cmd/godplugin
 		ARCHIVE="${FILE}.tar.gz"
 		tar -C bin -cvzf ${ARCHIVE} ${FILE/bin\//}
 		rm ${FILE}
